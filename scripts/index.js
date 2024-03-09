e8e711a1db33f0f2ce6effd3da6a68ba2a6d6366
Genderson Barinas Guio <genderson.barinas@gmail.com>
2024-03-09 11:35:20 -0500

    dark mode added

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,37 @@ hamButton.addEventListener('click', () => {
 	navElement.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+const cardsDarkMode = document.querySelectorAll(".card");
+const navigationListItems = document.querySelectorAll('ul.navigation li');
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "🔆";
+
+		cardsDarkMode.forEach(card => {
+			card.classList.toggle('card-dark-mode');
+		  });
+
+		navigationListItems.forEach(li => {
+			li.classList.toggle('li-dark-mode');
+		  });
+
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "🕶️";
+
+		cardsDarkMode.forEach(card => {
+			card.classList.toggle('card-dark-mode');
+		  });
+
+		navigationListItems.forEach(li => {
+			li.classList.toggle('li-dark-mode');
+		  });
+	}
+});
