311813856de7b63389f97d24b326a0e405df37db
Genderson Barinas Guio <genderson.barinas@gmail.com>
2024-03-25 18:20:55 -0500

    weather activity

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -2,7 +2,7 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = "https://api.openweathermap.org/data/3.0/onecall?lat=49.75&lon=6.64&appid={value}"
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=60b13e27acb47777c1591d386c6c8261&units=imperial"
 
 async function apiFetch(url) {
     try {
@@ -10,6 +10,7 @@ async function apiFetch(url) {
         if (response.ok){
             const data = await response.json();
             console.log(data);
+            displayResults(data);
         }
         else{
             throw Error(await response.test());
@@ -21,3 +22,12 @@ async function apiFetch(url) {
 }
 
 apiFetch(url);
+
+function displayResults(data){
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+}   
